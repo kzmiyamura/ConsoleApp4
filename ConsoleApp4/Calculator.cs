1caa9715a5b67a0443b64915314039278ecb67d2
kzmiyamura <kzmiyamura@gmail.com>
2025-12-31 18:06:22 +0900

    first commit

---
+++ ConsoleApp4/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp4
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("数字を1つ入力してください:");
+            int x = int.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("もう1つ入力してください:");
+            int y = int.Parse(Console.ReadLine() ?? "0");
+
+            var calc = new Calculator();
+            int result = calc.Add(x, y);
+
+            Console.WriteLine($"結果: {result}");
+        }
+    }
+}
